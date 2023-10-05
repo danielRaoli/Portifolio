daa2bb17651489edc2b21d12c6da311c46c6b365
Daniel Raoli <danielraoli074@gmail.com>
2023-10-05 13:27:25 -0300

    profile update

--- Script/script.js
+++ Script/script.js
@@ -1,47 +1,23 @@
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    const menuButton = document.getElementById('menu-button');
-    const menu = document.getElementById('menu');
-    const close = document.getElementById('btn-close')
+const menuButton = document.querySelector('#menu-button');
+const menu = document.querySelector('#menu');
+const close = document.querySelector('#btn-close')
 
-    menuButton.addEventListener('click', function() {
-      if (menu.style.display === 'none') {
-        menu.style.display = 'flex';
-        close.style.display = 'block'
+menuButton.addEventListener('click', () =>  {
+  if (menu.style.display === 'none') {
+    menu.style.display = 'flex';
+    close.style.display = 'block'
 
-      } else {
-        menu.style.display = 'none';
-        close.style.display = 'none'
-      }
-    });
-    close.addEventListener('click', ()=> {
-      if(close.style.display != 'none'){
-        menu.style.display = 'none'
-        close.style.display ='none'
-      }
-    })
+  } else {
+    menu.style.display = 'none';
+    close.style.display = 'none'
+  }
+});
+close.addEventListener('click', () => {
+  if (close.style.display != 'none') {
+    menu.style.display = 'none'
+    close.style.display = 'none'
+  }
+})
 
-    const swiper = new Swiper('.swiper', {
-      // Optional parameters
-      direction: 'vertical',
-      loop: true,
-    
-      // If we need pagination
-      pagination: {
-        el: '.swiper-pagination',
-      },
-    
-      // Navigation arrows
-      navigation: {
-        nextEl: '#btn-right',
-        prevEl: '#btn-left',
-      },
-    
-      // And if we need scrollbar
-      scrollbar: {
-        el: '.swiper-scrollbar',
-      },
-    });
-    
-  })
